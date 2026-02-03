113ed9c2f2da5c5a7b59bdbd486e33431634646e
Boris Giznevsky <mobile.369.app@gmail.com>
2026-02-03 14:43:21 -0800

    Initial Commit

---
+++ Neighbors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neighbors
+//
+//  Created by Boris Giznevsky on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
